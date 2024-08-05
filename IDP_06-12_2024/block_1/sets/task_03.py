ea250b44aadf6e8305eef60f9200804ba3f3c3f8
a.butvin <smart_07_08@mail.ru>
2024-08-05 09:37:31 +0300

    IDP_06-12_2024: block_1 fix remarks 2

--- IDP_06-12_2024/block_1/sets/task_03.py
+++ IDP_06-12_2024/block_1/sets/task_03.py
@@ -8,10 +8,17 @@
 
 def get_set_str(full_str):
     show = set()
-    return ''.join([sub_str for sub_str in full_str if not (sub_str in show or show.add(sub_str))])
+    result = []
+    for sub_str in full_str:
+        lower_sub_str = sub_str.lower()
+        if lower_sub_str not in show:
+            show.add(lower_sub_str)
+            result.append(sub_str)
+    return ''.join(result)
+    #return ''.join([sub_str for sub_str in full_str if not (sub_str in show or show.add(sub_str))])
 
 
-string_1 = "q we qwew erert"
+string_1 = "Vfvf vskf hfve"
 
 print(get_set_str(string_1))
 
